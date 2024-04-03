8bf7f1e8e62db355fcb53b3768fec764b30592dc
Mirko Mälicke <mirko@hydrocode.de>
2024-04-03 12:09:58 +0200

    handle scaling and compressino dynamically

--- processor/resample.py
+++ processor/resample.py
@@ -1,17 +1,39 @@
 """
 Resample a given GeoTiff to a specific spatial resolution.
 """
+from typing import Union, Literal, Optional
 import rasterio
+import pyproj
 from rasterio.coords import BoundingBox
-from rasterio.enums import Resampling
+from rasterio.enums import Resampling, Compression
+
+
+def auto_scale_factor(raster: rasterio.DatasetReader, target_resolution: float = 0.04, referece_epsg: int = 3857) -> float:
+    # TODO: hardcode the target crs for now
+    if raster.crs.to_epsg() != referece_epsg:
+        transform = pyproj.Transformer.from_crs(raster.crs, pyproj.CRS.from_epsg(referece_epsg), always_xy=True)
+
+        # transform the bounds
+        tbounds = transform.transform_bounds(*raster.bounds)
+    else:
+        tbounds = raster.bounds
+    
+    # calculate the resolutions in meters
+    xres = (tbounds[2] - tbounds[0]) / raster.width
+    yres = (tbounds[3] - tbounds[1]) / raster.height
+    
+    # divide the smaller resolution by the target resolution to get the scale factor
+    return min(xres, yres) / target_resolution
 
 
 def resample(
     input_file: str,
     output_file: str,
-    scale_factor: float = 1 / 10,
+    scale_factor: Union[float, Literal['auto']] = 1 / 10,
     method: Resampling = Resampling.bilinear,
     driver: str = "GTiff",
+    compress: Optional[Compression] = None,
+    jpeg_quality: Optional[int] = None
 ) -> BoundingBox:
     """
     Resample the input_file to the given scale_factor and save the output to output_file.
@@ -20,6 +42,13 @@ def resample(
 
     """
     with rasterio.open(input_file) as src:
+        # check the scale factor
+        if isinstance(scale_factor, str):
+            if scale_factor.lower() == 'auto':
+                scale_factor = auto_scale_factor(src)
+            else:
+                raise ValueError("Invalid value for scale_factor. Must be a float or 'auto'")
+
         # resample data to target shape while reading
         data = src.read(
             out_shape=(
@@ -60,8 +89,17 @@ def resample(
             crs=src.crs,
             transform=transform,
         )
+
+        # check if a compression is specified
+        if compress is not None:
+            write_options["compress"] = compress.upper()
+            if compress.lower() == 'jpeg':
+                write_options[jpeg_quality] = jpeg_quality if jpeg_quality is not None else 90
+        
+        # write the file
         with rasterio.open(output_file, "w", **write_options) as dst:
             dst.write(data)
         
+        # return a read-only reference to the file
         with rasterio.open(output_file, 'r') as dst:
             return dst.bounds
